2ae42ff2118b7130d9b8a9dde5d25bc08a348229
aekmekci72 <anna.ekmekci@gmail.com>
2024-05-29 12:42:10 -0400

    e

--- studybuddy/src/firebaseSetup.js
+++ studybuddy/src/firebaseSetup.js
@@ -1,20 +1,21 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-import serviceAccount from './serviceKey.json'; 
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const firebaseConfig = {
-  credential: firebase.credential.cert(serviceAccount),
-  databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`,
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+const auth = getAuth(app);
 
-try {
-  firebase.initializeApp(firebaseConfig);
-} catch (error) {
-  console.error('Firebase initialization error:', error);
-}
-
-export const auth = firebase.auth();
-export const firestore = firebase.firestore(); 
-
-export default firebase;
+export { app, auth };
