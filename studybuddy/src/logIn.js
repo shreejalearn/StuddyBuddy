96de546b23c73d3d00b5058954ae8fcac5aed953
shreejalearn <shreeja.das.16@gmail.com>
2024-05-29 19:21:32 -0400

    .

--- studybuddy/src/logIn.js
+++ studybuddy/src/logIn.js
@@ -12,23 +12,6 @@ const Login = () => {
     const onSubmit = async (e) => {
         e.preventDefault();
 
-    //     await signInWithEmailAndPassword(auth, email, password)
-    //         .then((userCredential) => {
-    //             // Signed in
-    //             const user = userCredential.user;
-    //             console.log(user);
-    //             localStorage.setItem('userName', email);
-
-    //             navigate("/mygallery");  // or any other route you want to navigate to
-    //         })
-    //         .catch((error) => {
-    //             const errorCode = error.code;
-    //             const errorMessage = error.message;
-    //             console.log(errorCode, errorMessage);
-    //         });
-    // };
-
-    // Login component
     await signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
         // Signed in
