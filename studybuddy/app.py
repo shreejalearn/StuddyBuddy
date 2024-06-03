26ff31d4f0e361a13a56665f30bdc4b595c64160
aekmekci72 <anna.ekmekci@gmail.com>
2024-06-02 22:11:24 -0400

    generation of meaningful questions and answers for practice test! component on the way

--- studybuddy/app.py
+++ studybuddy/app.py
@@ -1,46 +1,3 @@
-# from flask import Flask, request, jsonify
-# from flask_cors import CORS
-# from PIL import Image
-# import pytesseract
-# import io
-# import os
-# import firebase_admin
-# from firebase_admin import credentials, firestore
-
-# app = Flask(__name__)
-# CORS(app)
-
-# # Specify the full path to the Tesseract executable
-# pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-
-# # Initialize Firebase Admin SDK
-# cred = credentials.Certificate("serviceKey.json")
-# firebase_admin.initialize_app(cred)
-
-# # Initialize Firestore client
-# db = firestore.client()
-
-# @app.route('/recognize', methods=['POST'])
-# def recognize_handwriting():
-#     if 'image' not in request.files:
-#         return jsonify({'error': 'No image uploaded'})
-
-#     image_file = request.files['image']
-#     image_stream = io.BytesIO(image_file.read())
-#     image = Image.open(image_stream)
-
-#     recognized_text = pytesseract.image_to_string(image)
-
-#     # Store recognized_text in Cloud Firestore
-#     doc_ref = db.collection('recognized_texts').document()
-#     doc_ref.set({'text': recognized_text})
-
-#     return jsonify({'text': recognized_text})
-
-# if __name__ == '__main__':
-#     app.run(host='localhost', port=5000, debug=True)
-
-
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import firebase_admin
@@ -62,7 +19,16 @@ import googleapiclient.discovery
 from googleapiclient.discovery import build
 from youtube_transcript_api import YouTubeTranscriptApi
 from urllib.parse import urlparse, parse_qs
-
+from typing import OrderedDict
+from sentence_transformers import SentenceTransformer
+from transformers import T5ForConditionalGeneration, T5Tokenizer, BertTokenizer, BertModel, AutoTokenizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import torch
+import spacy
+from transformers import BertTokenizer as BTokenizer, BertModel as BModel
+from warnings import filterwarnings as ignore_warnings
+from transformers import pipeline
 
 import json
 
