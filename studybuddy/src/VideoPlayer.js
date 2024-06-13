90fed8e31ae547463c66dc8cad796de497b6c5cb
aekmekci72 <anna.ekmekci@gmail.com>
2024-06-12 22:50:22 -0400

    vid thing

--- studybuddy/src/VideoPlayer.js
+++ studybuddy/src/VideoPlayer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import './styles/vidstuff.css';
 
 const VideoPlayer = () => {
   // Extracting the videoPath from URL params
@@ -8,7 +9,7 @@ const VideoPlayer = () => {
   return (
     <div className="video-list">
       <div className="video-stuff">
-        <video controls>
+        <video controls className="video-player">
           <source src={`http://localhost:5000/videos/${videoPath}`} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
